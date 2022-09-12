7303a181e1cfeb75928a611447d59103a34fdedc
hrostmy <s.hromosiak@gmail.com>
2022-09-12 15:26:37 +0300

    registration posts

--- post/forms.py
+++ post/forms.py
@@ -1,4 +1,12 @@
 from django.contrib.auth.forms import PasswordChangeForm
+from .models import Post
+from django.forms import ModelForm
 
 class MyChangeFormPassword(PasswordChangeForm):
     pass
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = ['photo','text']
+
--- post/views.py
+++ post/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .forms import PostForm
 # Create your views here.
-
+@login_required
 def create(request):
-    pass
+    error = ''
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+        else:
+            error = 'Uncorrect form'
+    form = PostForm()
+
+    data = {
+        'form': form,
+        'error': error
+    }
+
+    return render(request, 'post/post_create.html')
