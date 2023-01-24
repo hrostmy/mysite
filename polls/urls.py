cbe128f3512de883ff35b97c8618af4d75d94149
stanislavbuldyhin <stanislavbuldyhin@geo-pay.net>
2023-01-24 17:23:19 +0200

    added poetry, gitignore. realized functoin for viewing user feed

--- polls/urls.py
+++ polls/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.feed_home, name='index'),
+    path('', views.feed, name='index'),
     path('logout/', views.logout_view, name='logout'),
     path('main/', views.main_page, name='main_page'),
     path('sign_in/', views.sign_in_view, name='sign_in'),
     path('register/', views.register, name='register'),
     path('profile/<str:username>', views.profile, name='profile'),
-    path('follow/<int:pk>', views.follow, name='follow')
+    path('follow/<int:pk>', views.follow, name='follow'),
+    path('<str:username>/', views.feed, name='user_feed')
 ]
